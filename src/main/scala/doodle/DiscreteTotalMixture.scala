d73de3e62527a358d4af1013adaab45f87b96fc6
Stephane Bersier <stephane.bersier@gmail.com>
2019-07-09 18:52:52 -0700

    Improved DiscreteTotalMixture code

--- src/main/scala/doodle/DiscreteTotalMixture.scala
+++ src/main/scala/doodle/DiscreteTotalMixture.scala
@@ -2,42 +2,75 @@ package doodle
 
 object DiscreteTotalMixture extends App {
 
-  for (i <- 0 to 13) {
+  for (i <- 0 to 50) {
     val r = row(pointCount = i, radius = i)
     println(i + ": " + r.sum + " " + r.toList)
   }
 
   def row(pointCount: Int, radius: Int): Array[Long] = {
+    require (pointCount >= 0)
+    require (radius >= 0)
+
     val histogram = Array.ofDim[Long](2*radius + 1)
 
     if (pointCount > 0) {
-      rem(0, Nil, pointCount, radius)
+      if (radius > 0) {
+        rem0(start = 0, positions = Nil, pointCount, radius)
+      }
+      else {
+        histogram(0) += pointCount
+      }
     }
 
-    def rem(start: Int, positions: List[Int], pointCount: Int, distance: Int) {
-//      require (pointCount >= 1)
-//      require (distance >= 0)
-//      require (start < histogram.length)
+    def rem0(start: Int, positions: List[Int], pointCount: Int, distance: Int) {
+//      require(pointCount >= 1)
+//      require(distance >= 0)
+//      require(start >= 0)
+//      require(start < radius)
 
-      val startDistance = math.abs(start - radius)
+      val startDistance = radius - start
       if (startDistance <= distance) {
         val newPositions = start :: positions
         if (pointCount == 1) {
-          for (p <- newPositions) {
-            histogram(p) += 1
-          }
+          addPoints(newPositions)
         }
         else {
-          rem(start, newPositions, pointCount - 1, distance - startDistance)
-        }
-
-        val newStart = start + 1
-        if (newStart < histogram.length) {
-          rem(newStart, positions, pointCount, distance)
+          rem0(start, newPositions, pointCount - 1, distance - startDistance)
         }
       }
-      else if (start < radius) {
-        rem(start + 1, positions, pointCount, distance)
+      val nextStart = start + 1
+      if (nextStart == radius) {
+        rem1(nextStart, positions, pointCount, distance)
+      }
+      else {
+        rem0(nextStart, positions, pointCount, distance)
+      }
+    }
+
+    def rem1(start: Int, positions: List[Int], pointCount: Int, distance: Int) {
+//      require(pointCount >= 1)
+//      require(distance >= 0)
+//      require(start >= radius)
+//      require(start < histogram.length)
+//      require(pointCount * (start - radius) <= distance)
+
+      val startDistance = start - radius
+      val newPositions = start :: positions
+      if (pointCount == 1) {
+        addPoints(newPositions)
+      }
+      else {
+        rem1(start, newPositions, pointCount - 1, distance - startDistance)
+      }
+      val nextStart = start + 1
+      if (pointCount * (startDistance + 1) <= distance && nextStart < histogram.length) {
+        rem1(nextStart, positions, pointCount, distance)
+      }
+    }
+
+    def addPoints(positions: List[Int]) {
+      for (p <- positions) {
+        histogram(p) += 1
       }
     }
 
