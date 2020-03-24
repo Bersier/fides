030b8d21e89f796b2a549c8d0f0a72487f1e6ded
Stephane Bersier <stephane.bersier@gmail.com>
2020-03-24 09:48:19 -0700

    backup

--- src/main/scala/v4/Protos.scala
+++ src/main/scala/v4/Protos.scala
@@ -9,9 +9,10 @@ object Protos {
     */
   trait Fides
 
-  val Mailer     = new Address // Isn't this getting silly?
+  val Mailer     = new Address
   val Comparator = new Address
   val KeySmith   = new Address
+  val Witness    = new Address
   val Notary     = new Address
   val Compiler   = new Address
 }
--- src/main/scala/v4/Syntax.scala
+++ src/main/scala/v4/Syntax.scala
@@ -1,13 +1,16 @@
 package v4
 
+/**
+  *
+  */
 object Syntax {
   type Multiset[A] = Map[A, BigInt]
 
-  sealed trait Loc
+  sealed trait Loc // Loc[Val] ?
   type OutLoc = Loc
   type InLoc = Loc
 
-  sealed trait Val
+  sealed trait Val // Move Val and subtypes to its own file
   final class Address extends Val
 
   sealed trait Process
@@ -17,26 +20,31 @@ object Syntax {
 
   final case class Constant(value: Val, outLoc: OutLoc) extends Primitive
 
-  final case class Send(message: InLoc, to: InLoc) extends Primitive
+//  final case class Send(message: InLoc, to: InLoc) extends Primitive
 
   final case class Forward(inLoc: InLoc, outLoc: OutLoc) extends Primitive
 
-  /** Can only be used to broadcast to locs in this code, not external ones */
-  final case class Broadcast(inLoc: InLoc, outLoc: OutLoc) extends Primitive
+  /** Can only be used to broadcast to locs in this code, not external ones **/
+  // Forgot why we need this again. Write down example, next time.
+//  final case class Broadcast(inLoc: InLoc, outLoc: OutLoc) extends Primitive
 
   final case class Wait(token: InLoc, inLoc: InLoc, outLoc: OutLoc) extends Primitive
 
   // Should Sign use a fixed signatory, similarly to (late) Receive (signatory: Signatory)?
-  final case class Sign(message: InLoc, signatory: InLoc, signedMessage: OutLoc) extends Primitive
-  final case class Unsign(signedMessage: InLoc, message: OutLoc, signatory: OutLoc) extends Primitive
+//  final case class Sign(message: InLoc, signatory: InLoc, signedMessage: OutLoc) extends Primitive
+//  final case class Unsign(signedMessage: InLoc, message: OutLoc, signatory: OutLoc) extends Primitive
+  // What if Unsign receives something that is not a signedMessage?
 
   final case class Join(first: InLoc, second: InLoc, pair: OutLoc) extends Primitive
-  final case class Fork(pair: InLoc, first: OutLoc, second: OutLoc) extends Primitive
+  final case class Split(pair: InLoc, first: OutLoc, second: OutLoc) extends Primitive
 
-  final case class Equals(first: InLoc, second: InLoc, equal: OutLoc) extends Primitive
+//  final case class Equals(first: InLoc, second: InLoc, equal: OutLoc) extends Primitive
 
   final case class Parallel(processes: Multiset[Process]) extends Process
   final case class Replicated(addresses: Set[Loc], process: Process) extends Process
+  // Semantics?
+
+  // final case class Scope? // to specify loc privacy
 
   final case class Awake(command: InLoc, process: Process) extends Process
   final case class Asleep(command: InLoc, process: Process) extends Process
