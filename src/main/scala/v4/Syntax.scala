523004c6ffd1682d019943e9c2dbb3cc24cd2451
Stephane Bersier <stephane.bersier@gmail.com>
2020-03-28 18:37:48 -0700

    Deleted stuff, and looks close to a solution (but looks  can be deceiving).

--- src/main/scala/v4/Syntax.scala
+++ src/main/scala/v4/Syntax.scala
@@ -22,6 +22,7 @@ object Syntax {
   /** Can only be used to broadcast to locs in this code, not external ones **/
   // Forgot why we need this again. Write down example, next time.
 //  final case class Broadcast(inLoc: InLoc, outLoc: OutLoc) extends Primitive
+  final case class Copy(inLoc: InLoc, outLocs: Multiset[OutLoc])
 
   final case class Wait(token: InLoc, inLoc: InLoc, outLoc: OutLoc) extends Primitive
 
@@ -29,35 +30,14 @@ object Syntax {
   final case class Split(pair: InLoc, first: OutLoc, second: OutLoc) extends Primitive
   
   final case class Concurrent(processes: Multiset[Process]) extends Process
-  final case class Replicated(addresses: Set[Loc], process: Process) extends Process
-  // Semantics?
+  final case class Replicated(broadcast: Set[InLoc], process: Process) extends Process // Added broadcast param here
+  // I'm thinking about how it's not possible to communicate to a replicated process otherwise... but do we really
+  // want that?
 
-  final case class New(addresses: Set[Loc], process: Process) // Scope. ? to specify loc privacy
+  final case class New(addresses: Set[Loc], process: Process)
   
   final case class Awake(name: Name, process: Process) extends Process
   final case class Asleep(name: Name, process: Process) extends Process
 
-  final case class Branch(boolean: InLoc, trueBranch: Process, falseBranch: Process) extends Process
-
-  final case class Swappable(location: InLoc, addresses: Set[Loc], process: Process) extends Process
-
-  // Swappable(process, address)
-  // Swap(process: InLoc, at: InLoc |?| Address)
-
-/*
-How to get a natural calculus for  Sleep, Kill, ...?
-
-Scenarios
- - Internal choice on receive
- - 
-
-Ambient calculus?
-*/
-
-  // Swap
-
-  /**
-    * For convenience, so that we don't need to reimplement basics.
-    */
-  final case class Execute[A, B](input: InLoc, function: A => B, output: OutLoc) extends Process
+  final case class Swappable(location: InLoc, process: Process) extends Process
 }
--- src/main/scala/v4/Values.scala
+++ src/main/scala/v4/Values.scala
@@ -1,27 +1,21 @@
 package v4
 
-sealed trait Val // Move Val and subtypes to its own file
+sealed trait Val
 sealed class Address extends Val
 final class Name extends Val
 
-// The advantage of making these addresses, instead of primitives, is that we don't have to worry about dangling
-// primitives (which would be needed to simulate these).
-// However, if they were local primitives, they would allow more explicit control...
-// They would make clear what can happen to this named scope...
-// In that case, however, the control primitives could not themselves be controlled...
 sealed trait Command extends Address
-final class Start(name: Name) extends Command // Play / Run / Execute / Resume
-final class Pause(name: Name) extends Command // Halt / Freeze
-final class Kill(name: Name) extends Command  // Kill / Stop / Terminate
+final class Start(name: Name) extends Command
+final class Pause(name: Name) extends Command
+final class Dissolve(name: Name) extends Command
+final class Kill(name: Name) extends Command
 
-// This one is a bit different from the others. To execute the others, one just needs to send 'unit' to them.
-// For this one, the destination must be specified.
 final class Move(scope: Name) extends Command
 
 
 sealed trait BoolVal extends Val
-object True extends Command
-object False extends Command
+object True extends BoolVal
+object False extends BoolVal
 
 final case class APair()
 
@@ -33,12 +27,4 @@ sealed trait Signatory extends Val
 
 final class SignatoryKey() extends Signatory {
   def signatory: Signatory = this
-}
-
-final case class ScalaVal[T] private(private val value: T) extends Val
-object ScalaVal {
-  implicit def pack[T](value: T): ScalaVal[T] = ScalaVal(value)
-  implicit def unpack[T](scalaVal: ScalaVal[T]): T = scalaVal match {
-    case ScalaVal(value) => value
-  }
 }
