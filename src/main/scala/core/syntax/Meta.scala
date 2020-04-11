3ed285dded17234a63b285cad8c44a85b4171e63
Stephane Bersier <stephane.bersier@gmail.com>
2020-04-10 19:26:22 -0700

    Backup (no red in meta)

--- src/main/scala/core/syntax/Meta.scala
+++ src/main/scala/core/syntax/Meta.scala
@@ -5,13 +5,15 @@ package core.syntax
 final case class Code[+K <: N, +C <: D, +C2 <: D, +T <: Lex[K, C, T]]
 (lex: Lex[CodeK[K, C], C2, T]) extends X[K, C, Code[K, C, C2, T]]
 
-final case class Escape[+K <: N, +C <: D, +T <: X[K, C, T]](expr: X[K, C, T]) extends X[CodeK[K, C], Val, T]
-// Need level for this, or need to repeat it, to escape code within code...
+/**
+  * Escapes one level of code.
+  */
+final case class Escape[+K <: N, +C <: D, +T <: X[K, C, T]](expr: T) extends X[CodeK[K, C], Val, T]
 
 /**
   * Matches Escape when level is zero. Otherwise, matches Epacse of a level lower by one.
   */
-final case class Epacse[S <: G](level: BigInt, expr: E[PattK[S]]) extends E[PattK[S]]
+final case class Epacse[+K <: N, +T <: X[K, Out, T]](level: BigInt, expr: T) extends X[CodeK[K, Out], Val, T]
 
 // It looks like we could actually move back to a flow syntax. Perhaps do that later once it's stable, as a form that
 // directly includes semantics.
