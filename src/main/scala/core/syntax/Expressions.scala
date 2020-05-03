4420c52aa55e19f71a46f2668a78ce26cf755d18
Stephane Bersier <stephane.bersier@gmail.com>
2020-05-02 18:04:21 -0700

    Only simple patterns, and some more Inp expressions to compensate...

--- src/main/scala/core/syntax/Expressions.scala
+++ src/main/scala/core/syntax/Expressions.scala
@@ -5,10 +5,15 @@ final case class AsValue[+K <: N, +T <: A, +C <: Loc[T]](loc: L[K, C]) extends A
 final case class APair[+K <: N, +C[+_ <: A] <: E, +T1 <: A, +T2 <: A, +C1 <: C[T1], +C2 <: C[T2]]
 (first: L[K, C1], second: L[K, C2]) extends A with L[K, C[APair[K, C, T1, T2, C1, C2]]]
 
-final case class Merge[+K <: N, +T <: A, +C[+_ <: A] <: E](
-  one: L[K, C[Bag[K, T, C]]],
-  two: L[K, C[Bag[K, T, C]]],
-) extends L[K, C[Bag[K, T, C]]]
+final case class Merge[+K <: N, +T <: A](one: L[K, Inp[Bag[K, T, Inp]]], two: L[K, Inp[Bag[K, T, Inp]]])
+  extends L[K, Inp[Bag[K, T, Inp]]]
+
+final case class Contains[+K <: N, +T <: A](bag: L[K, Inp[Bag[K, T, Inp]]], element: L[K, Inp[T]])
+  extends L[K, Inp[Bag[K, T, Inp]]]
+
+final case class Equals[+K <: N, +T <: A](one: L[K, Inp[T]], two: L[K, Inp[T]]) extends L[K, Inp[BoolVal]]
+
+final case class Branch[+K <: N, +T <: A](one: L[K, Out[T]], two: L[K, Out[T]]) extends L[K, Out[BoolVal]]
 
 final case class Signed[+K <: N, +T <: A, +C[+_ <: A] <: E] private(
   contents: L[K, C[T]],
@@ -28,10 +33,4 @@ object Signed {
 final case class ForgetInp[+K <: N](expr: L[K, Inp[_]]) extends L[K, Inp[U]]
 final case class ForgetOut[+K <: N](expr: L[K, Out[U]]) extends L[K, Out[Nothing]]
 
-final case class Copy[+K <: N, +T <: A, +C[+_ <: A] <: Out[_]](outs: L[K, Val[Bag[K, T, C]]]) extends L[K, C[T]]
-
-final case class All[+K <: N, +T <: A, +C[+_ <: A] <: Out[_]](outs: L[K, Val[Bag[K, T, C]]]) extends L[K, C[T]]
-final case class Any[+K <: N, +T <: A, +C[+_ <: A] <: Out[_]](outs: L[K, Val[Bag[K, T, C]]]) extends L[K, C[T]]
-final case class One[+K <: N, +T <: A, +C[+_ <: A] <: Out[_]](outs: L[K, Val[Bag[K, T, C]]]) extends L[K, C[T]]
-final case class Not[+K <: N, +C <: Out[_]](Out: L[K, C]) extends L[K, C]
-// Too complex... restrict to patterns that can be interpreted as shallow syntactic sugar for processes?
+final case class Copy[+K <: N, +T <: A](outs: L[K, Val[Bag[K, T, Out]]]) extends L[K, Out[T]]
