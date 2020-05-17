3c5b8276b1e794c685afaae66e8a045667cb4b9e
Stephane Bersier <stephane.bersier@gmail.com>
2020-05-17 12:44:38 -0700

    Dual of "Send"?

--- src/main/scala/core/syntax/Processes.scala
+++ src/main/scala/core/syntax/Processes.scala
@@ -2,7 +2,14 @@ package core.syntax
 
 final case class Send[+K <: N, T <: A](inp: L[K, Inp[T]], address: L[K, Inp[Code[_, Inp, Loc[T]]]])
   extends L[K, Prs]
+final case class Receive[+K <: N, T <: A](out: L[K, Inp[T]], dual(address): L[K, Inp[Code[_, Inp, Loc[T]]]])
+  extends L[K, Prs]
+// Can be achieve using two sends: Send(<a>, <b>)|Send(r, <a>)|Forward(a, out)
+//                            for: Forward(r, b)|Receive(out, <b>)
+
 final case class Forward[+K <: N, T <: A](inp: L[K, Inp[T]], out: L[K, Out[T]]) extends L[K, Prs]
+
+// Make inp and out first order?
 
 final case class Launch[+K <: N](
   code: L[K, Inp[Code[K, Inp, Prs]]],
@@ -10,6 +17,8 @@ final case class Launch[+K <: N](
 ) extends L[K, Prs]
 
 // TODO Reflect?
+
+final case class After(inp: Any, out1: Any, out2: Any)
 
 final case class Replicated[+K <: N](process: L[K, Prs]) extends L[K, Prs]
 final case class New[+K <: N](iDs: L[K, Val[Bag[_, IdeT, Val]]], process: L[K, Prs]) extends L[K, Prs]
