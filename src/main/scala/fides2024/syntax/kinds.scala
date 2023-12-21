8ddffba3351c02dd62e0b980fb37f7ec59f5aa2b
Stephane Bersier <stephane.bersier@gmail.com>
2023-12-21 13:22:34 -0500

    Simplified kind sealing

--- src/main/scala/fides2024/syntax/kinds.scala
+++ src/main/scala/fides2024/syntax/kinds.scala
@@ -1,10 +1,10 @@
 package fides2024.syntax
 
-trait Component(using SyntaxSeal)
+trait Component private[syntax]()
 // todo shouldn't T be contravariant when P is Negative?
-trait Expr[+P <: Polarity, +T <: ValSup](using SyntaxSeal) extends Component
+trait Expr[+P <: Polarity, +T <: ValSup] extends Component
 sealed trait ValSup
-trait Val[+T <: ValSup](using SyntaxSeal) extends Expr[Neutral, T], ValSup
+trait Val[+T <: ValSup] extends Expr[Neutral, T], ValSup
 
 sealed trait Polarity
 sealed trait Positive extends Polarity
@@ -16,6 +16,3 @@ type Opposite[P <: Polarity] = P match
   case Negative => Positive
   case Neutral => Neutral
   case Opposite[p] => p
-
-private[syntax] sealed trait SyntaxSeal
-private[syntax] given SyntaxSeal with {}
