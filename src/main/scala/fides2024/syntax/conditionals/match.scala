a2b2489dab6223c935db04390ef072c0e8bf200e
Stephane Bersier <stephane.bersier@gmail.com>
2024-01-15 10:35:07 -0500

    Only kept one Switch version, deleted the others, and moved definitions into connectors package

--- src/main/scala/fides2024/syntax/conditionals/match.scala
+++
@@ -1,8 +0,0 @@
-package fides2024.syntax.conditionals
-
-import fides2024.syntax.code.{Code, Ptrn, ValType, Xctr}
-
-/**
-  * Tries to match a value to the given pattern. Upon failure, outputs the value to the alternative instead.
-  */
-final case class Match[T <: ValType](pattern: Code[Ptrn[T, T]], alternative: Code[Xctr[T]]) extends Xctr[T]
