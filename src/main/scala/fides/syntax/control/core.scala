76bb912c8217adfa2ea2fd692193ffc04729bb5e
Stephane Bersier <stephane.bersier@gmail.com>
2024-02-24 11:26:24 -0500

    Tried to improve control primitives a little

--- src/main/scala/fides/syntax/control/core.scala
+++ src/main/scala/fides/syntax/control/core.scala
@@ -1,9 +1,9 @@
 package fides.syntax.control
 
 import fides.syntax.code.{Atom, Code, Expr, Process, Val, ValQ, ValType, Xctr}
-import fides.syntax.identifiers.{InpChan, OutChan}
+import fides.syntax.identifiers.{Cell, OutChan}
 import fides.syntax.meta.Quoted
-import fides.syntax.values.Pulse
+import fides.syntax.values.{Bool, Pulse}
 
 sealed trait Order extends Atom, ValQ[Order]
 case object Kill extends Order
@@ -11,19 +11,13 @@ case object Pause extends Order
 case object Start extends Order
 
 /**
-  * A controllable process that is awake
+  * A pausable process
   *
-  * A controllable process can be paused, (re)started, and killed.
+  * @param awake indicates the current state of the process (whether it's awake or asleep)
+  * @param body the process that can be paused
   */
-final case class Awake(leash: Code[InpChan[Order]], body: Code[Process]) extends Process
+final case class Pausable(awake: Code[Cell[Bool]], body: Code[Process]) extends Process
 // todo shouldn't there be a way to know when an order is completed?
-
-/**
-  * A controllable process that is asleep
-  *
-  * A controllable process can be paused, (re)started, and killed.
-  */
-final case class Asleep(leash: Code[InpChan[Order]], body: Code[Process]) extends Process
 
 /**
   * Upon reception of a pulse, the body's execution is started.
@@ -31,9 +25,19 @@ final case class Asleep(leash: Code[InpChan[Order]], body: Code[Process]) extend
 final case class OnHold(startSignal: Code[Expr[Pulse]], body: Code[Process]) extends Process
 
 /**
+  * A killable process
+  *
   * Upon reception of a pulse, the body's execution is stopped.
+  *
+  * @param killSignal upon reception of a pulse, the process is stopped
+  * @param deathSignal upon termination, a pulse is emitted
+  * @param body the process that can be stopped
   */
-final case class Mortal(killSignal: Code[Expr[Pulse]], body: Code[Process]) extends Process
+final case class Mortal(
+  killSignal: Code[Expr[Pulse]],
+  deathSignal: Code[Xctr[Pulse]],
+  body: Code[Process],
+) extends Process
 
 /**
   * No message can reach and/or exit the sandboxed process directly. The monitor serves as the intermediate.
