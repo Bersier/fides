b158029d3994758596f2d793104971244184be9f
Stephane Bersier <stephane.bersier@gmail.com>
2024-05-07 17:36:23 -0400

    Compiler crashes...

--- src/main/scala/util/tdict.scala
+++ src/main/scala/util/tdict.scala
@@ -2,27 +2,32 @@ package util
 
 import java.util.concurrent.atomic.AtomicInteger
 import scala.annotation.targetName
+import scala.compiletime.ops.any.==
 import scala.compiletime.ops.int.<=
+import scala.util.NotGiven
 
 object TDicts:
-  opaque type ID = Int
-  object Identifier:
-    def newInstance: ID =
+  type ID[+I <: Int] = I // todo opaque
+  object ID:
+    def newInstance: ID[Int] =
       val result = next.getAndIncrement()
-      if result == -1 then throw Error("Ran out of ints to represent identifiers.")
+      if result == 0 then throw Error("Ran out of ints to represent identifiers.")
       result
-    private val next = AtomicInteger(0)
-    given CanEqual[ID, ID] = CanEqual.derived
-  end Identifier
+    private val next = AtomicInteger(256)
+    val first: ID[0] = 0
+    val second: ID[1] = 1
+    given CanEqual[ID[Int], ID[Int]] = CanEqual.derived
+  end ID
 
-  opaque type TDict[T <: TList[V[?]]] = T
+  opaque type TDict[+T <: TList[V[?]]] = T
   object TDict:
     def empty: TDict[TList.Empty] = TList.Empty
   end TDict
   extension [T <: TList[V[U]], U](self: TDict[T])
     def size: Int = self.length
     @targetName("plus")
-    def +[E >: U, A <: E](key: ID, value: A): TDict[Extended[E, T, key.type, A]] =
+    def +[E >: U, A <: E](key: ID[Int], value: A)(using ContainsKey2[T, key.type] =:= false):
+    TDict[Extended[E, T, key.type, A]] =
       def extended(d: TList[V[E]]): TList[V[E]] = d match
         case TList.Empty => TList.Cons((key, value), TList.Empty)
         case TList.Cons((k, v), tail) => if key <= k
@@ -40,17 +45,28 @@ object TDicts:
       case TList.Cons[V[E], (k2, a2), tail2] => k1 <= k2 match
         case true => TList.Cons[V[E], (k1, a1), Merged[E, tail1, L2]]
 
-  type Extended[E, L <: TList[V[E]], K <: ID, A <: E] <: TList.Cons[V[E], ?, ?] = L match
+  type Extended[E, L <: TList[V[E]], K <: ID[Int], A <: E] <: TList.Cons[V[E], ?, ?] = L match
     case TList.Empty => TList.Cons[V[A], (K, A), TList.Empty]
     case TList.Cons[V[E], (k, a), tail] => K <= k match
       case true  => TList.Cons[V[E], (K, A), L]
       case false => TList.Cons[V[E], (k, a), Extended[E, tail, K, A]]
 
-  private type V[T] = (ID, T)
+  type ContainsKey[L <: TList[V[?]], I <: ID[Int]] <: Boolean = L match
+    case TList.Empty => false
+    case TList.Cons[V[Any], (I, Any), TList[(ID[Int], Any)]] => true
+    case TList.Cons[V[Any], (ID[Int], Any), tail] => ContainsKey[tail, I]
+
+  type ContainsKey2[L <: TList[V[?]], I <: ID[Int]] <: Boolean = L match
+    case TList.Empty => false
+    case TList.Cons[V[Any], (j, Any), tail] => I == j match
+      case true => true
+      case false => ContainsKey2[tail, I]
+
+  private type V[T] = (ID[Int], T)
 end TDicts
 
-trait TDictOps[TDict]:
-  extension (d: TDict)
-    def size: Int
-
-end TDictOps
+def test: Unit =
+  import TDicts.*
+  val empty: TDict[TList.Empty] = TDict.empty
+  val d1 = empty + (ID.first, 0)
+  val d2 = d1 + (ID.second, 0)
