15c5b3a71d9145d41d26c4f31204474643b8b840
Stephane Bersier <stephane.bersier@gmail.com>
2024-08-25 15:53:26 -0400

    Restricted Unwrapping to quotes of values

--- src/main/scala/fides/syntax/meta/wrapping.scala
+++ src/main/scala/fides/syntax/meta/wrapping.scala
@@ -8,12 +8,16 @@ import fides.syntax.code.{Code, Expr, Ptrn, Val, ValType, Xctr}
 final case class Wrap[T <: ValType](value: Code[Expr[T]]) extends Expr[Quoted[Val[T]]]
 
 /**
-  * Evaluates a Quoted.
+  * Unwraps a quoted value.
   */
-final case class UnWrap[T <: ValType](value: Code[Xctr[T]]) extends Xctr[Quoted[Expr[T]]]
+final case class UnWrap[T <: ValType](value: Code[Xctr[T]]) extends Xctr[Quoted[Val[T]]]
+
+/**
+  * Evaluates a quoted expression.
+  */
+final case class Eval[T <: ValType](value: Code[Expr[Quoted[Expr[T]]]]) extends Expr[T]
 
 /**
   * Pattern for unwrapping
   */
 final case class MatchWrap[P <: N, N <: ValType](value: Code[Ptrn[P, N]]) extends Ptrn[Quoted[Val[P]], Quoted[Val[N]]]
-// todo should this really be allowed?
