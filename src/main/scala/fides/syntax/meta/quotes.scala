f016e662342dc511373f0f0310b061a49e1f47da
Stephane Bersier <stephane.bersier@gmail.com>
2024-09-01 13:36:48 -0400

    Removed SignedMatcher, added documentation to escape-related code types, removed old todo, ...

--- src/main/scala/fides/syntax/meta/quotes.scala
+++ src/main/scala/fides/syntax/meta/quotes.scala
@@ -14,6 +14,10 @@ final case class Quoted[+S <: CodeType](code: Code[S]) extends Val[Quoted[S]], V
   *
   * Doesn't have a special meaning within [[MatchQuote]]s.
   *
+  * [[Escape]] never needs to be matched against, as it cannot appear in a [[Quoted]].
+  *
+  * To quote an [[Escape]] from a nested quote, rather than escaping the top-level quote, use [[QuotedEscape]].
+  *
   * (At the top-level (outside of a quote), could represent macro code.)
   */
 final case class Escape[S <: CodeType](code: Code[Expr[Quoted[S]]]) extends Code[S]
@@ -24,6 +28,12 @@ final case class Escape[S <: CodeType](code: Code[Expr[Quoted[S]]]) extends Code
   *
   * Upon launching of the [[Quoted]], [[QuotedEscape]] of level zero is converted to an [[Escape]].
   * Higher-level [[QuotedEscape]]s have their level lowered by one.
+  *
+  * Except perhaps for macro use, [[level]] should be smaller than quote nesting depth,
+  * ensuring that the [[QuotedEscape]] gets converted to an [[Escape]] before the quote actually gets executed.
+  *
+  * When [[QuotedEscape]] is used within [[MatchQuote]], nothing special happens.
+  * So [[QuotedEscape]]s within quotes are matched by [[QuotedEscape]]s within [[MatchQuote]], as expected.
   */
 final case class QuotedEscape[S <: CodeType](
   code: Code[Expr[Quoted[S]]],
@@ -35,19 +45,29 @@ final case class QuotedEscape[S <: CodeType](
   * (as well as nested [[Quoted]] and [[Quote]]).
   *
   * Doesn't have a special meaning within [[Quote]]s.
+  *
+  * To quote a [[MatchEscape]] from a nested [[MatchQuote]], rather than escaping the top-level [[MatchEscape]],
+  * use [[MatchEscapeMatcher]].
   */
 final case class MatchEscape[S <: CodeType](code: Code[Ptrn[Quoted[S], Quoted[S]]]) extends Code[S]
 // todo lossy type
 
 /**
   * Allows matching a [[MatchEscape]](Matcher) within a [[MatchQuote]]. See also [[SignedMatcher]].
+  *
+  * Since [[MatchEscape]] escapes [[MatchQuote]], a new type of code is needed to match a [[MatchEscape]].
+  * And to match that new type of code, yet a different one is needed, and so forth.
+  * This is solved by having a level, effectively introducing a hierarchy of matchers.
+  * This is similar to having to use a backslash in a regex to escape another backslash
+  * (except that the number of backslashes needed grows exponentially with how meta the regex is).
+  *
+  * [[MatchEscapeMatcher]]`(c, 0)` matches [[MatchEscape]]`(c)`.
+  * For `level > 0`, [[MatchEscapeMatcher]]`(c, level)` matches [[MatchEscapeMatcher]]`(c, level - 1)`.
   */
 final case class MatchEscapeMatcher[S <: CodeType](
   code: Code[Ptrn[Quoted[S], Quoted[S]]],
   level: Code[Val[NaturalNumber]] = NaturalNumber(0),
 ) extends Code[S]
-// todo EscapeMatcher could be merged with Escape (which would then have an additional level parameter)
-//  (and similarly for other matchers)
 
 /**
   * Analogous to s-Strings in Scala, but for code
--- src/main/scala/fides/syntax/signatures/core.scala
+++ src/main/scala/fides/syntax/signatures/core.scala
@@ -21,26 +21,6 @@ object Signed:
 end Signed
 
 /**
-  * Since Signed values cannot be created freely, a different type of value is needed for matching.
-  * To match the latter, yet a different one is needed, and so forth.
-  * This is solved by having a level, effectively introducing a hierarchy of matchers.
-  * This is similar to having to use a backslash in a regex to escape another backslash
-  * (except that the number of backslashes needed grows exponentially with how meta the regex is).
-  *
-  * [[SignedMatcher]](0, m, s) matches [[Signed]](m, s).
-  * For level > 0, [[SignedMatcher]](level, m, s) matches [[SignedMatcher]](level - 1, m, s).
-  *
-  * @tparam T keeps track of the value type
-  */
-final case class SignedMatcher[T <: ValType](
-  document: Code[Val[T]],
-  signature: Code[Val[Identifier]],
-  level: Code[Val[NaturalNumber]] = NaturalNumber(0),
-) extends Val[Signed[T]]
-// todo should only be allowed in code patterns (although maybe it's not such a big deal if it can be used elsewhere)
-// todo delete? Can it always be simulated with MatchEscape(MatchWrap(MatchSign(...)))?
-
-/**
   * Primitive to sign values
   */
 final case class Sign[T <: ValType](
@@ -63,4 +43,4 @@ final case class MatchSign[P <: N, N <: ValType](
   document: Code[Ptrn[P, N]],
   signature: Code[Ptrn[Identifier, Identifier]],
 ) extends Ptrn[Signed[P], Signed[N]]
-// todo should this really be allowed?
+// todo fix type, similarly to MatchPair
