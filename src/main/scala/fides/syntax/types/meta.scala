bdbe97271b498d93a1e5822206c61b84adc9950a
Bersier <stephane.bersier@gmail.com>
2025-12-02 18:32:44 -0500

    Baby steps

--- src/main/scala/fides/syntax/types/meta.scala
+++ src/main/scala/fides/syntax/types/meta.scala
@@ -1,11 +1,20 @@
 package fides.syntax.types
 
-type TopQ = GenericQ[TopS]
-sealed trait GenericQ[+S <: TopS]
+type TopQ = Q[TopS]
 
-final abstract class NormalQ[+S <: TopS] extends GenericQ[S]
+sealed trait Q[+S <: TopS]
 
-final abstract class EscapeQ[+S <: TopS, +Q <: TopQ, Level] extends GenericQ[S]
+final abstract class EscapeQ[S <: TopS, +ES <: Polar2[QuotedT[S], ?], Level] extends Q[S]
+
+final abstract class PairQ[
+  T1 <: TopT,
+  T2 <: TopT,
+  +P <: TopP,
+  +S1 <: Polar2[T1, P],
+  +S2 <: Polar2[T2, P],
+  +Q1 <: Q[S1],
+  +Q2 <: Q[S2],
+] extends Q[PairS[T1, T2, P, S1, S2]]
 
 // todo variance
 // todo add M here instead?
