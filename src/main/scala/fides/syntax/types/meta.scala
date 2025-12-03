8ff5a23d35eb005921da31082c6c3c8edb00327c
Bersier <stephane.bersier@gmail.com>
2025-12-03 11:04:44 -0500

    A little tightening

--- src/main/scala/fides/syntax/types/meta.scala
+++ src/main/scala/fides/syntax/types/meta.scala
@@ -6,28 +6,12 @@ sealed trait Scape[+S <: TopS, +M <: TopM]
 
 final abstract class QuoteC[
   S <: TopS, P <: TopP, M <: TopM,
-  C <: Scape[S, SomeM[P, M]], RC <: Scape[S, SomeM[P, BotM]],
+  +C <: Scape[S, SomeM[P, M]], +RC <: Scape[S, SomeM[P, BotM]],
 ](using TrimmedR[S, C, RC]) extends Scape[QuoteS[S, P, RC], M]
 
-sealed trait TrimmedR[S <: TopS, C <: Scape[S, TopM], RC <: Scape[S, SomeM[TopP, BotM]]]
-// todo not sure if we should keep the type parameter S
-object TrimmedR:
-  given [
-    T1 <: TopT, T2 <: TopT, P <: TopP,
-    S1 <: Polar2[T1, P], S2 <: Polar2[T2, P], H <: TopP, M <: TopM,
-    C1 <: Scape[S1, SomeM[H, M]], C2 <: Scape[S2, SomeM[H, M]],
-    RC1 <: Scape[S1, SomeM[H, BotM]], RC2 <: Scape[S2, SomeM[H, BotM]],
-  ] => (TrimmedR[S1, C1, RC1], TrimmedR[S2, C2, RC2]) => TrimmedR[
-    PairS[T1, T2, P, S1, S2],
-    PairC[T1, T2, P, S1, S2, SomeM[H, M], C1, C2],
-    PairC[T1, T2, P, S1, S2, SomeM[H, BotM], RC1, RC2],
-  ]()
-end TrimmedR
-
 final abstract class EscapeC[
-  S <: TopS, P <: TopP,
-  ES <: Polar2[QuotedT[S], P], M <: TopM, // todo note that ES cannot just be any TopS...
-  +C <: Scape[ES, M],
+  S <: TopS, P <: TopP, M <: TopM,
+  +C <: Scape[? <: Polar2[QuotedT[S], P], M],
 ] extends Scape[S, SomeM[P, M]]
 // todo Level
 
