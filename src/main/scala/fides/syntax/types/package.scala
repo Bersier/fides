b043fb661ae291908513f7b38097b58e7eb4f1ba
Bersier <stephane.bersier@gmail.com>
2025-12-05 18:38:13 -0500

    A little more documentation

--- src/main/scala/fides/syntax/types/package.scala
+++ src/main/scala/fides/syntax/types/package.scala
@@ -4,5 +4,11 @@ package fides.syntax
   * This package provides the type machinery to express the syntax of Fides.
   *
   * There are four main levels of "types":
+  * <ol>
+  * <li> <b>Data types</b> characterize data/values.
+  * <li> <b>Grammar types</b> characterize code, except for escapes.
+  * <li> <b>Meta types</b> are also called scapes; they fully characterize code, including escapes.
+  * <li> <b>Code types</b> represent actual code.
+  * </ol>
   */
 package object types
