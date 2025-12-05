d77c6afb17cbf144d09e65d09defb2e95c843a38
Bersier <stephane.bersier@gmail.com>
2025-12-05 06:58:53 -0500

    Deleted TopN.ReprR and TopN.DoubleR

--- src/main/scala/util/phantom.scala
+++ src/main/scala/util/phantom.scala
@@ -15,17 +15,4 @@ object TopN:
   final abstract class Z extends TopN
   type `0` = Z
   type `1` = S[Z]
-  
-  sealed trait ReprR[N <: TopN, B <: Bits]
-  object ReprR:
-    given ReprR[TopN.Z, Bits.None]
-    given [N <: TopN, B <: Bits, D <: TopN] => ReprR[N, B] => DoubleR[N, D] => ReprR[D, Bits.Some[Bit.O, B]]
-    given [N <: TopN, B <: Bits, D <: TopN] => ReprR[N, B] => DoubleR[N, D] => ReprR[TopN.S[D], Bits.Some[Bit.I, B]]
-  end ReprR
-  
-  sealed trait DoubleR[N <: TopN, D <: TopN]
-  object DoubleR:
-    given DoubleR[TopN.Z, TopN.Z]
-    given [N <: TopN, D <: TopN] => DoubleR[N, D] => DoubleR[TopN.S[N], TopN.S[TopN.S[D]]]
-  end DoubleR
 end TopN
