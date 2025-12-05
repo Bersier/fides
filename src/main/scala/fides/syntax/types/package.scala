9db3e3996b7726bf9fd3b12a3ba37a8afb6068cb
Bersier <stephane.bersier@gmail.com>
2025-12-05 16:40:43 -0500

    Started adding documentation to the types package itself.

---
+++ src/main/scala/fides/syntax/types/package.scala
@@ -0,0 +1,8 @@
+package fides.syntax
+
+/**
+  * This package provides the type machinery to express the syntax of Fides.
+  *
+  * There are four main levels of "types":
+  */
+package object types
