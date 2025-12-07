242820f2af5c1bad99c1383f23aff32bcbedacbc
Bersier <stephane.bersier@gmail.com>
2025-12-07 18:31:11 -0500

    Generalized TrimmedMR recursion, so that it can take care of Qs.

--- src/main/scala/fides/syntax/machinery/trimming.scala
+++ src/main/scala/fides/syntax/machinery/trimming.scala
@@ -3,46 +3,54 @@ package fides.syntax.machinery
 type TrimmedR[
   G <: TopG, P <: TopP, Q <: TopQ,
   M <: ConsM[G, ConsQ[P, Q]], TM <: ConsM[G, ConsQ[P, BotQ]],
-] = TrimmedMR[TopN.`1`, M, TM]
+] = TrimmedMR[TopN.`1`, ConsQ[P, Q], ConsQ[P, BotQ], M, TM]
 
 /**
-  * `G <: `[[TopG]]`, Q <: `[[TopQ]]`, TQ <: Q, H <: `[[TopN]]`, M <: `[[ConsM]]`[G, Q], TM <: `[[ConsM]]`[G, TQ]`
+  * `G <: `[[TopG]]`, TQ <: Q, M <: `[[ConsM]]`[G, Q], TM <: `[[ConsM]]`[G, TQ]`
   */
-sealed trait TrimmedMR[-H <: TopN, M <: TopM, TM <: TopM]
+sealed trait TrimmedMR[-H <: TopN, Q <: TopQ, TQ <: TopQ, M <: ConsM[TopG, Q], TM <: ConsM[TopG, TQ]]
 object TrimmedMR:
 
   /**
-    * Note that in this case, [[TrimmedR]]`[G, P, Q, M, ITM]` and [[TrimmedR]]`[G, HTM, TM]` should also hold.
+    * Note that in this case,
+    * [[TrimmedR]]`[G, P, Q, M, ITM]` and [[TrimmedR]]`[G, P, TQ, TM, ITM]` should also hold.
     *
-    * `HTQ <: Q`
+    * `TQ <: Q`
     */
   given [
-    G <: TopG, P <: TopP, Q <: TopQ, TQ <: TopQ,
+    G <: TopG, P <: TopP,
     ITM <: ConsM[G, ConsQ[P, BotQ]],
-    H <: TopN,
+    H <: TopN, Q <: TopQ, TQ <: TopQ,
     M <: ConsM[G, ConsQ[P, Q]],
     TM <: ConsM[G, ConsQ[P, TQ]],
-  ] => (
-    TrimmedMR[TopN.S[H], M, TM],
-  ) => TrimmedMR[
-    H,
+  ] => TrimmedMR[
+    TopN.S[H], ConsQ[P, Q], ConsQ[P, TQ],
+    M,
+    TM,
+  ] => TrimmedMR[
+    H, Q, TQ,
     QuoteM[G, P, ITM, Q, M],
     QuoteM[G, P, ITM, TQ, TM],
   ]
 
   given [
     G <: TopG,
-    M <: EscapeM[G, TopQ],
-  ] => TrimmedMR[TopN.Z, M, ConsM[G, BotQ]]
+    Q <: TopQ,
+    M <: EscapeM[G, Q],
+  ] => TrimmedMR[
+    TopN.Z, Q, BotQ,
+    M,
+    ConsM[G, BotQ],
+  ]
 
   /**
     * `TQ <: Q`
     */
   given [
-    G <: TopG, Q <: TopQ, TQ <: TopQ,
-    H <: TopN,
+    G <: TopG,
+    H <: TopN, Q <: TopQ, TQ <: TopQ,
   ] => TrimmedQR[H, Q, TQ] => TrimmedMR[
-    H,
+    H, Q, TQ,
     EscapeM[G, Q],
     EscapeM[G, TQ],
   ]
@@ -51,12 +59,12 @@ object TrimmedMR:
     * `TQ <: Q`
     */
   given [
-    G <: TopG, Q <: TopQ, TQ <: TopQ,
-    H <: TopN,
+    G <: TopG,
+    H <: TopN, Q <: TopQ, TQ <: TopQ,
     M <: EscapeM[G, Q],
     TM <: EscapeM[G, TQ],
-  ] => (TrimmedQR[H, Q, TQ], TrimmedMR[H, M, TM]) => TrimmedMR[
-    TopN.S[H],
+  ] => TrimmedMR[H, Q, TQ, M, TM] => TrimmedMR[
+    TopN.S[H], ConsQ[BotP, Q], ConsQ[BotP, TQ],
     EscapeM.Step[G, Q, M],
     EscapeM.Step[G, TQ, TM],
   ]
@@ -65,12 +73,12 @@ object TrimmedMR:
     * `TQ <: Q`
     */
   given [
-    G <: TopG, P <: TopP, Q <: TopQ, TQ <: TopQ,
-    H <: TopN,
+    G <: TopG, P <: TopP,
+    H <: TopN, Q <: TopQ, TQ <: TopQ,
     M <: ConsM[PolarG[QuoteD[G], P], Q],
     TM <: ConsM[PolarG[QuoteD[G], P], TQ],
-  ] => (TrimmedQR[H, Q, TQ], TrimmedMR[H, M, TM]) => TrimmedMR[
-    TopN.S[H],
+  ] => TrimmedMR[H, Q, TQ, M, TM] => TrimmedMR[
+    TopN.S[H], ConsQ[P | GenP[BotB, BotB, TopB], Q], ConsQ[P | GenP[BotB, BotB, TopB], TQ],
     EscapeM.Head[G, P, Q, M],
     EscapeM.Head[G, P, TQ, TM],
   ]
@@ -81,23 +89,25 @@ object TrimmedMR:
   given [
     D1 <: TopD, D2 <: TopD, P1 <: TopP, P2 <: TopP,
     G1 <: PolarG[D1, P1], G2 <: PolarG[D2, P2],
-    H1 <: TopP, H2 <: TopP, Q1 <: TopQ, Q2 <: TopQ, TQ1 <: TopQ, TQ2 <: TopQ,
-    H <: TopN,
-    M1 <: ConsM[G1, ConsQ[H1, Q1]], M2 <: ConsM[G2, ConsQ[H2, Q2]],
-    TM1 <: ConsM[G1, ConsQ[H1, TQ1]], TM2 <: ConsM[G2, ConsQ[H2, TQ2]],
-  ] => (TrimmedMR[H, M1, TM1], TrimmedMR[H, M2, TM2]) => TrimmedMR[
-    H,
-    PairM[D1, D2, P1, P2, G1, G2, ConsQ[H1, Q1],  ConsQ[H2, Q2], M1, M2],
-    PairM[D1, D2, P1, P2, G1, G2, ConsQ[H1, TQ1], ConsQ[H2, TQ2], TM1, TM2],
+    H <: TopN, Q1 <: TopQ, Q2 <: TopQ, TQ1 <: TopQ, TQ2 <: TopQ,
+    M1 <: ConsM[G1, Q1], M2 <: ConsM[G2, Q2],
+    TM1 <: ConsM[G1, TQ1], TM2 <: ConsM[G2, TQ2],
+  ] => (TrimmedMR[H, Q1, TQ1, M1, TM1], TrimmedMR[H, Q2, TQ2, M2, TM2]) => TrimmedMR[
+    H, Q1 | Q2, TQ1 | TQ2,
+    PairM[D1, D2, P1, P2, G1, G2, Q1, Q2, M1, M2],
+    PairM[D1, D2, P1, P2, G1, G2, TQ1, TQ2, TM1, TM2],
   ]
 
   /**
-    * The rule below is admissible (and probably unhelpful for actual inference).
+    * The rule below is admissible (and probably harmful for actual inference).
     * It is here only for documentation purposes.
+    *
+    * `TQ <: Q, TTQ <: TQ`
     */
   protected given [
-    H <: TopN, M <: ConsM[TopG, TopQ], TM <: ConsM[TopG, TopQ], TTM <: ConsM[TopG, TopQ],
-  ] => (TrimmedMR[TopN.S[H], M, TM], TrimmedMR[H, TM, TTM]) => TrimmedMR[H, M, TTM]
+    H <: TopN, Q <: TopQ, TQ <: TopQ, TTQ <: TopQ,
+    M <: ConsM[TopG, Q], TM <: ConsM[TopG, TQ], TTM <: ConsM[TopG, TTQ],
+  ] => (TrimmedMR[TopN.S[H], Q, TQ, M, TM], TrimmedMR[H, TQ, TTQ, TM, TTM]) => TrimmedMR[H, Q, TTQ, M, TTM]
 end TrimmedMR
 
 /**
@@ -105,8 +115,13 @@ end TrimmedMR
   */
 sealed trait TrimmedQR[Height <: TopN, Q <: TopQ, TQ <: TopQ]
 object TrimmedQR:
+
   given [Q <: TopQ] => TrimmedQR[TopN.Z, Q, BotQ]
+
+  /**
+    * `TQ <: Q`
+    */
   given [
-    H <: TopN, P <: TopP, Q <: TopQ, TQ <: Q,
+    H <: TopN, P <: TopP, Q <: TopQ, TQ <: TopQ,
   ] => TrimmedQR[H, Q, TQ] => TrimmedQR[TopN.S[H], ConsQ[P, Q], ConsQ[P, TQ]]
 end TrimmedQR
