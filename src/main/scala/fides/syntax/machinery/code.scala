1dfca8d3f74bc43f52d5f4409894a4ce608cd78b
Bersier <stephane.bersier@gmail.com>
2025-12-08 08:02:18 -0500

    Moved Code given to companion object

--- src/main/scala/fides/syntax/machinery/code.scala
+++ src/main/scala/fides/syntax/machinery/code.scala
@@ -1,5 +1,7 @@
 package fides.syntax.machinery
 
+import fides.syntax.values.*
+
 /**
   * Parent type of all actual Fides code
   * <br><br>
@@ -8,6 +10,13 @@ package fides.syntax.machinery
   * type that is a supertype of that of actual escapes.
   */
 trait Code[M <: TopM] private[syntax]()
+object Code:
+  given [
+    D1 <: TopD, D2 <: TopD, P1 <: TopP, P2 <: TopP,
+    G1 <: PolarG[D1, P1], G2 <: PolarG[D2, P2], Q1 <: TopQ, Q2 <: TopQ,
+    M1 <: ConsM[G1, Q1], M2 <: ConsM[G2, Q2],
+  ] => (c1: Code[M1], c2: Code[M2]) => Code[PairM[D1, D2, P1, P2, G1, G2, Q1, Q2, M1, M2]] = Pair(c1, c2)
+end Code
 
 @deprecated
 trait OldCode[+G <: TopG] private[syntax]()
