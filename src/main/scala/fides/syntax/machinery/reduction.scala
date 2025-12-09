0b930468235d8717e2edc7beee024f83ef0d9479
Bersier <stephane.bersier@gmail.com>
2025-12-09 07:42:28 -0500

    Backup 10; added some comments to reduction.scala

--- src/main/scala/fides/syntax/machinery/reduction.scala
+++ src/main/scala/fides/syntax/machinery/reduction.scala
@@ -1,9 +1,22 @@
 package fides.syntax.machinery
 
 // todo I believe we have the property that at height H, the P in TQ is BotP; double-check and document
+
+/**
+  * Reduction type-evaluates the escapes that end one level above [[H]].
+  *
+  * @tparam H unary encoding of the level at which the reduction is to occur
+  * @tparam Q polarity stack for [[M]]
+  * @tparam TQ polarity stack for [[TM]]
+  * @tparam M scape to be reduced at [[H]]
+  * @tparam TM reduced [[M]]
+  */
 sealed trait MReductionR[-H <: TopN, Q <: TopQ, TQ <: TopQ, M <: ConsM[TopG, Q], TM <: ConsM[TopG, TQ]]
 object MReductionR:
 
+  /**
+    * Inductive case for [[QuoteM]]
+    */
   given [
     P <: TopP, `P'` <: TopP, ITM <: TopM, TITM <: TopM,
     H <: TopN, Q <: TopQ, TQ <: TopQ,
@@ -19,6 +32,9 @@ object MReductionR:
     QuoteM[`P'`, TQ, TITM, TM],
   ]
 
+  /**
+    * Base case for [[EscapeM.Head]] when `H` is zero
+    */
   given [
     TG <: TopG, TQ <: TopQ,
     TM <: ConsHM[TG, TQ], P <: TopP, Q <: TopQ, // todo P and Q are loose
@@ -29,20 +45,29 @@ object MReductionR:
     TM,
   ]
 
+  /**
+    * Base case for [[EscapeM.Step]] when `H` is zero
+    */
+  given [
   // todo Q is very loose. But maybe that's okay? Maybe as long as the relation is a superset of the required one,
   //  and is tight for the Ms, it's okay?
-  given [
     Q <: TopQ,
     M <: EscapeM.Step[? <: TopG, Q, ?, ?, ?],
   ] => MReductionR[TopN.Z, ConsQ[BotP, Q], ConsQ[BotP, Q], M, M]
   // todo remove ` <: TopG`?
 
+  /**
+    * Base case for escape matchers when `H` is zero
+    */
   given [TG <: TopG, Q <: TopQ, TM <: ConsHM[TG, TopQ], M <: TopM] => MReductionR[
     TopN.Z, Q, Q & ConsQ[BotP, TopQ],
     EscapeM[TG, Q, TM, M],
     ConsM[TG, Q & ConsQ[BotP, TopQ]],
   ]
 
+  /**
+    * Inductive case for [[EscapeM.Head]] (when `H` is non-zero)
+    */
   given [
     ITG <: TopG, `ITG'` <: TopG,
     ITM <: ConsHM[ITG, TopQ], `ITM'` <: ConsHM[`ITG'`, TopQ], P <: TopP, `P'` <: TopP,
@@ -55,6 +80,9 @@ object MReductionR:
     EscapeM.Head[`ITG'`, `ITM'`, `P'`, TQ, TM],
   ]
 
+  /**
+    * Inductive case for [[EscapeM.Step]] (when `H` is non-zero)
+    */
   given [
     H <: TopN,
     ITG <: TopG, `ITG'` <: TopG,
@@ -67,12 +95,18 @@ object MReductionR:
     EscapeM.Step[`ITG'`, TQ, `EITM'`, TEM, TM],
   ]
 
+  /**
+    * Inductive case for escape matchers (when `H` is non-zero)
+    */
   given [H <: TopN, TG <: TopG, Q <: TopQ, TM <: ConsHM[TG, TopQ], M <: TopM] => MReductionR[
     TopN.S[H], Q, Q,
     EscapeM[TG, Q, TM, M],
     EscapeM[TG, Q, TM, M],
   ]
 
+  /**
+    * Inductive case for [[PairM]]
+    */
   given [
     D1 <: TopD, D2 <: TopD, P1 <: TopP, P2 <: TopP,
     G1 <: PolarG[D1, P1], G2 <: PolarG[D2, P2],
@@ -86,11 +120,26 @@ object MReductionR:
   ]
 end MReductionR
 
+/**
+  * `TQ <: Q`
+  *
+  * @tparam H unary encoding for the trim level
+  * @tparam Q polarity stack to be trimmed
+  * @tparam TQ trimmed [[Q]]
+  */
 sealed trait QTrimmingR[H <: TopN, Q <: TopQ, TQ <: TopQ]
 object QTrimmingR:
 
+  /**
+    * Base case: when fully trimmed (to zero height), any polarity stack simply reduces to the trivial stack.
+    */
   given [Q <: TopQ] => QTrimmingR[TopN.Z, Q, BotQ]
 
+  /**
+    * Straightforward inductive case.
+    *
+    * `TQ <: Q`
+    */
   given [
     H <: TopN, P <: TopP, Q <: TopQ, TQ <: TopQ,
   ] => QTrimmingR[H, Q, TQ] => QTrimmingR[TopN.S[H], ConsQ[P, Q], ConsQ[P, TQ]]
