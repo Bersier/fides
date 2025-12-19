0d8e41bbf6c481ccbf7d991c36517756f9679e71
Bersier <stephane.bersier@gmail.com>
2025-12-19 03:57:22 -0500

    More cases, two todos

--- src/main/scala/fides/syntax/machinery/meta2.scala
+++ src/main/scala/fides/syntax/machinery/meta2.scala
@@ -25,37 +25,59 @@ final case class Quote2M[
 todo
 Reducing escapes
 
+todo isn't ReducedG = G? In that case, shouldn't it be directly derivable from ReducedM via m2g,
+ without requiring additional transformation?
+ If so, the additional transformation would have to be lifted to be applied to M' somehow.
+todo shouldn't W be involved somehow? So the G can adjust based on required variance as needed?
+ Well, isn't that's why the given for the neutrals would have higher priority,
+ but the others can also apply to a Ntrl? So no W, but polarity is taken into account.
+
 C1 || G1 <: Expr(? <: QuoteD(M')), M' tight
 Escape(a, C1) = C || M G H
-Quote(a, ... C ...)  =>
+QuoteExpr(a, ... C ...)  =>
   ReducedM = M', G = +m2g(M') | -H; requiring +m2g(M') <: +H
 
 C1 || G1 <: Xctr(? >: QuoteD(M')), M' tight
 Escape(a, C1) = C || M G H
-Quote(a, ... C ...)  =>
+QuoteXctr(a, ... C ...)  =>
   ReducedM = M', G = +m2g(M') & +H; requiring +m2g(M') >: -H
+
+C1 || G1 <: Ntrl(? =: QuoteD(M')), M' tight
+Escape(a, C1) = C || M G H
+QuoteNtrl(a, ... C ...)  =>
+  ReducedM = M', G = +m2g(M'); requiring -H <: m2g(M') <: +H
 
 ----
 
 C1 || G1 <: Expr(? <: QuoteD(M')), M' tight
 Escape(a, C1) = C || M G H
-Quote(a, ... QuoteExpr(b, ... C ...) ...)  =>
+QuoteExpr(a, ... QuoteExpr(b, ... C ...) ...)  =>
   ReducedM = M', G = +m2g(M') | -H; requiring +m2g(M') <: +H
 
 C1 || G1 <: Expr(? <: QuoteD(M')), M' tight
 Escape(a, C1) = C || M G H
-Quote(a, ... QuoteXctr(b, ... C ...) ...)  =>
+QuoteExpr(a, ... QuoteXctr(b, ... C ...) ...)  =>
   ReducedM = M', G = -m2g(M') & +H; requiring +m2g(M') <: +H
 
 C1 || G1 <: Xctr(? >: QuoteD(M')), M' tight
 Escape(a, C1) = C || M G H
-Quote(a, ... QuoteExpr(b, ... C ...) ...)  =>
+QuoteXctr(a, ... QuoteExpr(b, ... C ...) ...)  =>
   ReducedM = M', G = +m2g(M') & +H; requiring +m2g(M') >: -H
 
 C1 || G1 <: Xctr(? >: QuoteD(M')), M' tight
 Escape(a, C1) = C || M G H
-Quote(a, ... QuoteXctr(b, ... C ...) ...)  =>
+QuoteXctr(a, ... QuoteXctr(b, ... C ...) ...)  =>
   ReducedM = M', G = -m2g(M') | -H; requiring +m2g(M') >: -H
+
+C1 || G1 <: Ntrl(? =: QuoteD(M')), M' tight
+Escape(a, C1) = C || M G H
+QuoteNtrl(a, ... QuoteExpr(b, ... C ...) ...)  =>
+  ReducedM = M', G = +m2g(M'); requiring -H <: m2g(M') <: +H
+
+C1 || G1 <: Ntrl(? =: QuoteD(M')), M' tight
+Escape(a, C1) = C || M G H
+QuoteNtrl(a, ... QuoteXctr(b, ... C ...) ...)  =>
+  ReducedM = M', G = -m2g(M'); requiring -H <: m2g(M') <: +H
 
 ----
 
