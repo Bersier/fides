f2b966328e9576c401984a4c9af0adfe5b03fd44
Bersier <stephane.bersier@gmail.com>
2025-12-24 12:17:04 -0500

    Small improvement

--- src/main/scala/fides/syntax/mirroring.scala
+++ src/main/scala/fides/syntax/mirroring.scala
@@ -2,13 +2,14 @@ package fides.syntax
 
 import scala.annotation.unchecked.uncheckedVariance
 
-sealed trait TopH
-sealed trait `+H`[+G <: OffTopG] extends TopH
+type TopH = `+H`[OffTopG]
+sealed trait `+H`[+G <: OffTopG]
 final abstract class `-H`[-G <: OffTopG] extends `+H`[G @uncheckedVariance]
+type BotH = `-H`[OffTopG]
 
 type `-`[H <: TopH] <: TopH = H match
   case `-H`[g] => `+H`[g]
   case `+H`[EmptyArgsG] => `+H`[EmptyArgsG]
   case `+H`[NonEmptyArgsG[h, t]] => `+H`[NonEmptyArgsG[`-`[h], `-`[t]]]
-  // todo add more leaf cases
+  // todo add more leaf cases here
   case `+H`[g] => `-H`[g]
