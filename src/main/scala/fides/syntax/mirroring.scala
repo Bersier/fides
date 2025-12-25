eb0c1e606ad6c82bfc045d03ab4fd3f22f541c34
Bersier <stephane.bersier@gmail.com>
2025-12-25 10:29:24 -0500

    ...

--- src/main/scala/fides/syntax/mirroring.scala
+++ src/main/scala/fides/syntax/mirroring.scala
@@ -7,29 +7,22 @@ sealed trait `+H`[+G <: OffTopG]
 final abstract class `-H`[-G <: OffTopG] extends `+H`[G @uncheckedVariance] // todo...
 type BotH = `-H`[OffTopG]
 
-sealed trait HInvR[-H <: TopH, `-H` <: TopH]
+sealed trait HInvR[-H >: BotH <: TopH, `-H` >: BotH <: TopH]
+sealed trait HInvLR:
+  given [G <: TopG] => HInvR[`+H`[G] | BotH, `-H`[G]]
 object HInvR:
-  given `0`: [G <: OffTopG] => HInvR[`-H`[G], `+H`[G]]()
-  given `1`: HInvR[`+H`[EmptyArgsG], `+H`[EmptyArgsG]]()
+  given `0`: [G <: OffTopG] => HInvR[`-H`[G], `+H`[G] | BotH]()
+  given `1`: HInvR[`+H`[EmptyArgsG] | BotH, `+H`[EmptyArgsG] | BotH]()
   given `2`: [
+    HeadInv >: BotH <: TopH, TailInv >: `-H`[ArgsG] <: `+H`[ArgsG],
     Head >: BotH <: TopH, Tail >: `-H`[ArgsG] <: `+H`[ArgsG],
-  ] => HInvR[
-    `+H`[NonEmptyArgsG[Head, Tail]],
-    `+H`[
-      NonEmptyArgsG[HInv[Head] | BotH,
-      (HInv[Tail] | `-H`[fides.syntax.ArgsG]) & `+H`[fides.syntax.ArgsG]],
-    ],
+  ] => (HInvR[Head, HeadInv], HInvR[Tail, TailInv]) => HInvR[
+    `+H`[NonEmptyArgsG[Head, Tail]] | BotH,
+    `+H`[NonEmptyArgsG[HeadInv, TailInv]] | BotH,
   ]()
 end HInvR
-
-type HInv[H <: TopH] <: TopH = H match
-  case `-H`[g] => `+H`[g]
-  case `+H`[EmptyArgsG] => `+H`[EmptyArgsG]
-  case `+H`[NonEmptyArgsG[h, t]] => `+H`[NonEmptyArgsG[HInv[h], HInv[t]]]
-  // todo add more leaf cases here
-  case `+H`[g] => `-H`[g]
 
 type DInv[D >: BotD <: TopD] <: TopD = D match
   case EmptyRecordD => EmptyRecordD
   case NonEmptyRecordD[k, v, t] => NonEmptyRecordD[KInv[k], DInv[v], DInv[t]]
-  // todo add more cases
+  // todo...
