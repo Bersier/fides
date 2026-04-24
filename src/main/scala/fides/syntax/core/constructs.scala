62e983183200f2017bda265eceed761f9b55ca9a
Bersier <stephane.bersier@gmail.com>
2026-04-24 13:18:34 -0400

    More syntax primitives

--- src/main/scala/fides/syntax/core/constructs.scala
+++ src/main/scala/fides/syntax/core/constructs.scala
@@ -50,6 +50,8 @@ final case class BlockXctr(apolarBlock: Code, HeadExtractor: Code) extends Code
   */
 final case class Send(contents: Code, recipient: Code) extends Code
 
+final case class DivMod(dividend: Code, divisor: Code, quotient: Code, remainder: Code) extends Code
+
 /**
   * Behaviorally equivalent to an infinite number of copies of the given body
   *
@@ -62,7 +64,35 @@ final case class Repeated(body: Code) extends Code
   */
 final case class Concurrent(processes: Code) extends Code
 
-final case class DivMod(dividend: Code, divisor: Code, quotient: Code, remainder: Code) extends Code
+/**
+  * @param awake a Boolean cell whose value indicates whether the process is running (True) or paused (False)
+  */
+final case class Pausable(awake: Code, process: Code) extends Code
+
+/**
+   * Delays [[process]] until [[signal]] is received.
+   *
+   * Executes the body only if the received signal is [[True]].
+   * If [[False]], the body is discarded without getting executed.
+  */
+final case class Possible(signal: Code, process: Code) extends Code
+
+/**
+  * A killable process; apolar
+  *
+  * Upon reception of a pulse, the body's execution is stopped.
+  *
+  * @param signal when it arrives, kills the process
+  * @param process the process that can be stopped; apolar
+  */
+final case class Mortal(signal: Code, process: Code) extends Code
+
+/**
+  * @param signal to catch the process
+  * @param process that can be caught
+  * @param quote an extractor for the quote of the caught process state
+  */
+final case class Catchable(signal: Code, process: Code, quote: Code) extends Code
 
 //endregion - Apolars
 
@@ -89,8 +119,6 @@ final case class Address(name: Code, datatype: Code) extends Code
 final case class Entry(key: Code, value: Code) extends Code
 
 final case class Quote(code: Code) extends Code
-
-// Binary structors
 
 final case class Document(signatory: Code, contents: Code) extends Code
 
@@ -126,10 +154,51 @@ final case class SwapG(name1: Code, name2: Code, target: Code) extends Code
 
 //endregion - Other Reversible Polars
 
+//region ==== Other Expression Polars ====
+
+final case class Conjoin(conjuncts: Code) extends Code
+
+final case class Disjoin(disjuncts: Code) extends Code
+
+final case class RandomBit() extends Code
+
+final case class Sum(terms: Code) extends Code
+
+final case class Multiply(factors: Code) extends Code
+
+final case class Merge(bags: Code) extends Code
+
 /**
-  * An unordered collection of syntactic elements
+  * Replaces all the names in the quote by fresh names. Also removes all shadowing.
   */
-final case class Args(arguments: Multiset[Code]) extends Code
+final case class Freshen(quote: Code) extends Code
+
+/**
+  * Applies the given transformation to each child of the root of the given quote whose type is compatible,
+  * and outputs the updated quote.
+  *
+  * @param quote to be equivariantly transformed
+  * @param transformation to be applied to compatible children of the quote root node
+  */
+final case class Update(quote: Code, transformation: Code) extends Code
+
+/**
+  * Outputs the children of the root of the given quote, as a bag.
+  */
+final case class Children(quote: Code) extends Code
+
+/**
+  * Launches [[quote]] as a new process, and outputs a signed value (aka document) of the code, confirming the launch.
+  */
+final case class Launch(quote: Code) extends Code
+
+//endregion - Other Expression Polars
+
+//region ==== Other Extractor Polars ====
+
+final case class Inspect(signature: Code, payload: Code)
+
+//endregion - Other Extractor Polars
 
 /**
   * Akin to names in the pi-calculus
@@ -139,9 +208,26 @@ final case class Name(representation: Identifier) extends Code
 final val LauncherName = Name(launcherIdentifier)
 
 /**
+  * An unordered collection of syntactic elements
+  */
+final case class Args(arguments: Multiset[Code]) extends Code
+
+/**
   * Akin to `new` in the pi-calculus
   *
   * @param names that are new within the scope
   * @param body the body of the scope, in which the names are available
   */
 final case class New(names: Code, body: Code) extends Code
+
+/**
+  * An annotated piece of code. The annotation does not change the semantics of the code.
+  * It acts as a structured comment.
+  *
+  * If the annotation is bound to a quote, then it belongs to that quote.
+  *
+  * @param quoteName the name of the quote to which this quote is bound
+  * @param code the annotated code
+  * @param annotation the annotation; a value
+  */
+final case class Annotated(quoteName: Code, code: Code, annotation: Code) extends Code
