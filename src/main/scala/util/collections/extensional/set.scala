e958298ebc30fc9e3edc0cf6eaed4a1dc576df34
Bersier <stephane.bersier@gmail.com>
2026-04-27 09:06:52 -0400

    FiniteSet init fix

--- src/main/scala/util/collections/extensional/set.scala
+++ src/main/scala/util/collections/extensional/set.scala
@@ -15,26 +15,17 @@ case class FiniteSet[+T] private(protected val repr: Set[T @uncheckedVariance]) 
 object FiniteSet:
   def apply(): FiniteSet[Nothing] =
     new FiniteSet(Set.empty)
-  def apply(element: Any): FiniteSet[element.type] =
-    new FiniteSet(Set(element))
-  def apply(element1: Any, element2: Any): FiniteSet[element1.type | element2.type] =
-    new FiniteSet(Set(element1, element2))
-  def apply(element1: Any, element2: Any, element3: Any): FiniteSet[element1.type | element2.type | element3.type] =
-    new FiniteSet(Set(element1, element2, element3))
-  def apply[T](elements: T*): FiniteSet[T] =
-    new FiniteSet(elements.toSet)
+  def apply(element: Any): NonEmpty[element.type] =
+    new NonEmpty(Set(element))
+  def apply(element1: Any, element2: Any): NonEmpty[element1.type | element2.type] =
+    new NonEmpty(Set(element1, element2))
+  def apply(element1: Any, element2: Any, element3: Any): NonEmpty[element1.type | element2.type | element3.type] =
+    new NonEmpty(Set(element1, element2, element3))
+  def apply[T](element: T, elements: T*): NonEmpty[T] =
+    new NonEmpty(elements.toSet + element)
 
   class NonEmpty[+T](representation: Set[T @uncheckedVariance]) extends FiniteSet[T](representation):
     override def u[U](that: FiniteSet[U]): NonEmpty[T | U] =
       new NonEmpty(this.repr ++ that.repr)
-  object NonEmpty:
-    def apply(element: Any): NonEmpty[element.type] =
-      new NonEmpty(Set(element))
-    def apply(element1: Any, element2: Any): NonEmpty[element1.type | element2.type] =
-      new NonEmpty(Set(element1, element2))
-    def apply(element1: Any, element2: Any, element3: Any): NonEmpty[element1.type | element2.type | element3.type] =
-      new NonEmpty(Set(element1, element2, element3))
-    def apply[T](element: T, elements: T*): NonEmpty[T] =
-      new NonEmpty(elements.toSet + element)
   end NonEmpty
 end FiniteSet
